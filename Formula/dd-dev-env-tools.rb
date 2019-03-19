3eda290f2ab656b2a45cb79341738a4327073877
Stefan Schablowski <stefan.schablowski@desmodyne.com>
2019-03-19 10:15:46 +0100

    [#1]: update formula for DesmoDyne DevEnvTools to version 0.0.3

--- Formula/dd-dev-env-tools.rb
+++ Formula/dd-dev-env-tools.rb
@@ -13,15 +13,21 @@
 class DdDevEnvTools < Formula
   desc     "DesmoDyne DevEnvTools"
   homepage "https://github.com/desmodyne/devenvtools"
-  url      "https://github.com/desmodyne/devenvtools/archive/0.0.2.tar.gz"
-  sha256   "07a090935041d6470d26aff7b169567869b62810a245dd1e4b64e574ca8f2dce"
+  url      "https://github.com/desmodyne/devenvtools/archive/0.0.3.tar.gz"
+  sha256   "c677722e2bccd454646d5c0aae88bd06d619fb1a3203ffcb923af7de139f5428"
 
   # required to update default bash 3 to version 4 (or 5)
   depends_on "bash"
+  depends_on "git-flow-avh"
+  depends_on "gnu-sed"
   depends_on "jq"
   depends_on "yq"
 
+  # requires tap added using `brew tap desmodyne/tools`
+  depends_on "dd-bash-lib"
+
   def install
-    bin.install "code/bin/dd-download-repos"
+    bin.install      "code/bin/dd-download-repos"
+    pkgshare.install "conf/dd-download-repos.sample.yaml"
   end
 end
