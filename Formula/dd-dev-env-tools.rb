e7e269213f2be6670f3280b2442a34c337ded1a7
Stefan Schablowski <stefan.schablowski@desmodyne.com>
2019-06-08 20:26:42 +0200

    [#1]: review formula for DesmoDyne DevEnvTools at version 0.0.3

--- Formula/dd-dev-env-tools.rb
+++ Formula/dd-dev-env-tools.rb
@@ -12,12 +12,10 @@
 
 class DdDevEnvTools < Formula
   desc     "DesmoDyne DevEnvTools"
-  homepage "https://github.com/desmodyne/devenvtools"
-  url      "https://github.com/desmodyne/devenvtools/archive/0.0.3.tar.gz"
+  homepage "https://github.com/desmodyne/DevEnvTools"
+  url      "https://github.com/desmodyne/DevEnvTools/archive/0.0.3.tar.gz"
   sha256   "c677722e2bccd454646d5c0aae88bd06d619fb1a3203ffcb923af7de139f5428"
 
-  # required to update default bash 3 to version 4 (or 5)
-  depends_on "bash"
   depends_on "git-flow-avh"
   depends_on "gnu-sed"
   depends_on "jq"
