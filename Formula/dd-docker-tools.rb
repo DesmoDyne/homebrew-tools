9a22cfb9c9c8440ec9582aeb1916494393740cbd
Stefan Schablowski <stefan.schablowski@desmodyne.com>
2019-06-08 20:33:42 +0200

    [#1]: update formula for DesmoDyne DockerTools to version 0.0.2

--- Formula/dd-docker-tools.rb
+++ Formula/dd-docker-tools.rb
@@ -13,8 +13,8 @@
 class DdDockerTools < Formula
   desc     "DesmoDyne DockerTools"
   homepage "https://github.com/desmodyne/DockerTools"
-  url      "https://github.com/desmodyne/DockerTools/archive/0.0.1.tar.gz"
-  sha256   "504fbf4874aef838d0fe4135918c7d90c27a58b9ca902ded2e1399601e717197"
+  url      "https://github.com/desmodyne/DockerTools/archive/0.0.2.tar.gz"
+  sha256   "367e49c2cec4eb1e7f07004590bc6b24109ccc685084ddeafe120ed4ade09d00"
 
   depends_on "awscli"
   depends_on "coreutils"
