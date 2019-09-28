b57cb538df28c9f683e23a3531735d9cd01713a0
Bob the Builder <bob.the.builder@desmodyne.com>
2019-09-28 11:05:59 +0200

    [#1]: update formula for DesmoDyne VaultTools to version 0.0.2

--- Formula/dd-vault-tools.rb
+++ Formula/dd-vault-tools.rb
@@ -7,32 +7,27 @@
 # created : 2019-03-31
 
 
-# see also dd-bash-lib.rb
+# see also dd-bash-lib.rb and dd-docker-tools.rb.j2
 
 
 class DdVaultTools < Formula
   desc     "DesmoDyne VaultTools"
   homepage "https://github.com/desmodyne/VaultTools"
-  url      "https://github.com/desmodyne/VaultTools/archive/0.0.1.tar.gz"
-  sha256   "a4cba439b25f084d56674e3e389049aae778757e664c5cd9e57bce6d22fa8389"
+  url      "https://github.com/desmodyne/VaultTools/archive/0.0.2.tar.gz"
+  sha256   "a44dfcd65add33e68c7dbf93732cea705d8fff34a4a7abd9fab67869f48ab69c"
 
-  # TODO: these are only required for CI/CD test / upload / etc. scripts
+  # NOTE: only req'd for CI/CD
   # req'd for realpath, sha256sum
-  depends_on "coreutils"
-  depends_on "jinja2-cli"
+  # depends_on "coreutils"
+  # depends_on "jinja2-cli"
 
   # required to update default bash 3 to version 4 (or 5)
-  # TODO: introduce convention if this should be listed here:
-  # dd-bash-lib lists latest bash as dependency, but scripts
-  # of this project also make use of bash 4+ features, e.g. arrays
   depends_on "bash"
   depends_on "desmodyne/tools/dd-bash-lib"
   depends_on "jq"
   depends_on "vault"
   depends_on "yq"
 
-  # https://github.com/Homebrew/brew/blob/master/ ...
-  #  ... docs/Formula-Cookbook.md#just-moving-some-files
   def install
     bin.install Dir["code/bin/*"]
   end
