8484bd516b7747359b766c1baa561d7b5b429e6c
Bob the Builder <bob.the.builder@desmodyne.com>
2019-09-28 11:04:10 +0200

    [#1]: update formula for DesmoDyne BashLib to version 0.0.7

--- Formula/dd-bash-lib.rb
+++ Formula/dd-bash-lib.rb
@@ -13,6 +13,11 @@
 # https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
 # https://github.com/Homebrew/brew/blob/master/docs/How-to-Create-and-Maintain-a-Tap.md
 
+# TODO: audit the formula:
+# https://github.com/Homebrew/brew/blob/master/docs/ ...
+#  ... Formula-Cookbook.md#audit-the-formula
+# this currently fails to install to build some Ruby / gem native extensions:
+# https://github.com/Homebrew/brew/issues/5561
 
 # formula file and Ruby class name concept:
 # https://github.com/Homebrew/brew/blob/master/ ...
@@ -20,12 +25,42 @@
 
 class DdBashLib < Formula
   desc     "DesmoDyne BashLib"
-  homepage "https://github.com/desmodyne/bashlib"
-  url      "https://github.com/desmodyne/bashlib/archive/0.0.6.tar.gz"
-  sha256   "81fb90d5718a3e42d9316726f2500e087922b91544068a8520a197325b872118"
+  homepage "https://github.com/desmodyne/BashLib"
+  url      "https://github.com/desmodyne/BashLib/archive/0.0.7.tar.gz"
+  sha256   "460e9ec58d9b1e1fdd48851c0eaa8488f70b20ddecd30ba10d6ea78dc4ff71d2"
+
+  # TODO: introduce convention if to list dependencies that macOS also provides:
+  # curl, git, etc. might have to be installed if latest version is required
+
+  # https://github.com/Homebrew/brew/blob/master/docs/ ...
+  #  ... Formula-Cookbook.md#specifying-other-formulae-as-dependencies
+  # NOTE: bats is required by the .../cicd/bin/test script and jinja2 (from
+  # jinja2-cli brew package) for .../cicd/bin/upload; neither is needed
+  # nor used in production, i.e. after the dd-bash-lib package is installed;
+  # also, there is a mismatch between brew and DesmoDyne conventions:
+  #  1. BashLib is a shell library which is not 'built' at all
+  #     (as e.g. a C++ library would be compiled and linked)
+  #  2. in order to test BashLib using 'brew test', the .../test/bats/*.bats
+  #     would have to made available to production; however this would be
+  #     be done for testing alone and not to actually use the library
+  # therefore, neither brew build nor test concepts apply here at all:
+  #   depends_on "bats" => :test
+  #   depends_on "jinja2-cli" => :build
+  # and these packages won't be installed as dependency of this formula,
+  # but independently on the host that performs CI/CD test and upload:
+  # depends_on "bats"
+  # depends_on "jinja2-cli"
 
   # required to update default bash 3 to version 4 (or 5)
   depends_on "bash"
+  # req'd for ggrep
+  depends_on "grep"
+  # req'd for gdate, greadlink, realpath and others; sha256sum for CI/CD
+  depends_on "coreutils"
+  # req'd for gxargs
+  depends_on "findutils"
+  # req'd for gsed
+  depends_on "gnu-sed"
   depends_on "jq"
   depends_on "yq"
 
@@ -34,4 +69,12 @@ class DdBashLib < Formula
   def install
     lib.install "code/lib/dd-bash-lib.sh"
   end
+
+  # https://github.com/Homebrew/brew/blob/master/ ...
+  #  ... docs/Formula-Cookbook.md#add-a-test-to-the-formula
+  # TODO: find a way to 'brew test'; see comment above
+  # test do
+  #   # this fails; ${HOME} is empty
+  #   system "${HOME}/cicd/bin/test"
+  # end
 end
