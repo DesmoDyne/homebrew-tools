d25812d1fe6f0a4413a9d59325f5bc074aaed158
Bob the Builder <bob.the.builder@desmodyne.com>
2020-08-09 22:16:38 +0200

    [#1]: update formula for DesmoDyne ReleaseTools to version 0.0.2

--- Formula/dd-release-tools.rb
+++ Formula/dd-release-tools.rb
@@ -7,26 +7,23 @@
 # created : 2019-03-31
 
 
-# see also dd-bash-lib.rb and dd-docker-tools.rb.j2
+# see also *Tools/cicd/tmpl/<tool name>.rb.j2
 
 
 class DdReleaseTools < Formula
   desc     "DesmoDyne ReleaseTools"
   homepage "https://github.com/desmodyne/ReleaseTools"
-  url      "https://github.com/desmodyne/ReleaseTools/archive/0.0.1.tar.gz"
-  sha256   "b6de976d66e6a27b40f90ba2b9fdecfe19ab6d7f45683764a8c841f4b21864a9"
+  url      "https://github.com/desmodyne/ReleaseTools/archive/0.0.2.tar.gz"
+  sha256   "53df6d44a94068fc6a68cd9e098e5eb0c891d2441d0b81717b521d5e3e3902cb"
 
-  # NOTE: only req'd for CI/CD
-  # depends_on "jinja2-cli"
-
-  # required to update default bash 3 to version 4 (or 5)
-  depends_on "bash"
-  # req'd for realpath and others; sha256sum for CI/CD
+  # req'd for realpath and others
   depends_on "coreutils"
   depends_on "desmodyne/tools/dd-bash-lib"
+  depends_on "git"
   depends_on "jq"
 
   def install
     bin.install Dir["code/bin/*"]
+    lib.install Dir["code/lib/*"]
   end
 end
