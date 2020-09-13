061128b31096d031d2367a78970b5d58e31791d0
Bob the Builder <bob.the.builder@desmodyne.com>
2020-09-13 15:00:18 +0200

    [#1]: update formula for DesmoDyne VaultTools to version 0.0.3

--- Formula/dd-vault-tools.rb
+++ Formula/dd-vault-tools.rb
@@ -13,22 +13,20 @@
 class DdVaultTools < Formula
   desc     "DesmoDyne VaultTools"
   homepage "https://github.com/desmodyne/VaultTools"
-  url      "https://github.com/desmodyne/VaultTools/archive/0.0.2.tar.gz"
-  sha256   "a44dfcd65add33e68c7dbf93732cea705d8fff34a4a7abd9fab67869f48ab69c"
+  url      "https://github.com/desmodyne/VaultTools/archive/0.0.3.tar.gz"
+  sha256   "b7fc7e8ba69632c48a7399db57acb39c258e9626a6f139d3faa059c267b20d2b"
 
-  # NOTE: only req'd for CI/CD
-  # req'd for realpath, sha256sum
-  # depends_on "coreutils"
-  # depends_on "jinja2-cli"
-
-  # required to update default bash 3 to version 4 (or 5)
-  depends_on "bash"
+  # req'd for realpath and others
+  depends_on "coreutils"
   depends_on "desmodyne/tools/dd-bash-lib"
+  depends_on "git"
+  depends_on "jinja2-cli"
   depends_on "jq"
   depends_on "vault"
   depends_on "yq"
 
   def install
-    bin.install Dir["code/bin/*"]
+    bin.install      Dir["code/bin/*"]
+    pkgshare.install Dir["code/tmpl/*"]
   end
 end
